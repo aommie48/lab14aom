13f78b696a66a3f7d19bb305755a4c5b47365db5
lab14aom <ananya.aomsinxz@gmail.com>
2025-01-24 15:23:51 +0700

    edit

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -5,18 +5,14 @@ void insertionSort(T d[], int N) {
     for (int i = 1; i < N; i++) {
         T key = d[i];
         int j = i - 1;
-
-        // เลื่อนค่าที่น้อยกว่า key ไปข้างหน้า
         while (j >= 0 && d[j] < key) {
             d[j + 1] = d[j];
             j--;
         }
         d[j + 1] = key;
-
-        // แสดงผล Pass
         cout << "Pass " << i << ":";
         for (int k = 0; k < N; k++) {
-            cout << (k == 0 ? "" : " ") << d[k]; // เว้นวรรคเฉพาะระหว่างตัวเลข
+            cout << (k == 0 ? "" : " ") << d[k]; 
         }
         cout << endl;
     }
